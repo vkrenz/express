5d9c35b4854cab31215b6dfb91b8b96e05964550
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2022-10-03 23:04:51 -0400

    third commit

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -17,25 +17,31 @@ router.post('/', (req, res) => {
  * @see Always make sure to put dynamic routes BELOW any static routes! 
  * */
 
-router.route('/id')
+router.route("/:id")
     .get((req, res) => {
-
+        console.log(`Current User: ${req.user}`)
+        res.send(`Get User With ID ${req.params.id}`)
     })
     .put((req, res) => {
-
+        res.send(`Update User With ID ${req.params.id}`)
     })
     .delete((req, res) => {
-
+        res.send(`Delete User With ID ${req.params.id}`)
     })
+
+const users = [
+    { name: 'Victor' },
+    { name: 'John' }
+]
 
 /** 
  * @see param is a type of middleware inside JS.
  * @see middleware is what runs between the request being sent to your server and the actual response being returned.
- * @see @param next moves onto the next peice of middleware.
+ * @see @param next moves onto the next peice of middleware. (.get())
 */
-
-router.param('id', (req, res, next, id) => {
-
+router.param("id", (req, res, next, id) => {
+    req.user = users[id].name
+    next()
 })
 
 // router.get('/:id', (req, res) => {
@@ -45,6 +51,7 @@ router.param('id', (req, res, next, id) => {
 // router.put('/:id', (req, res) => {
 //     res.send(`Update User With ID ${req.params.id}`)
 // })
+
 // router.delete('/:id', (req, res) => {
 //     res.send(`Delete User With ID ${req.params.id}`)
 // })
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 /**
- * @version 1.1
+ * @version 1.2
+ * @date 3/10/2022
  * @see {@link https://www.youtube.com/watch?v=SccSCuHhOw0}
  */
 
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(logger)
 
 app.get('/', (req, res) => {
-    console.log("Hello World!");
+    console.log("Hello World!")
     res.render('index', {
         text: 'World!'
     })
@@ -16,5 +18,15 @@ app.get('/', (req, res) => {
 
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
+
+/**
+ * @function logger
+ * @description middleware function
+ * @see all middleware take req, res, next.
+ */
+ function logger(req, res, next) {
+    console.log(`Current URL: ${req.originalUrl}`)
+    next()
+}
 
 app.listen(8080)
